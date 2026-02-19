9c376acfa880883fc5ffd65c9aab3b3793c913b2
mrachek <mrachek@asocscloud.com>
2026-02-19 16:10:33 +0200

    added linked list insert and delete functions

--- linked-lists.ts
+++ linked-lists.ts
@@ -28,6 +28,52 @@ class LinkedList {
         return this;
     }
 
+    insert(position, value) {
+        if (position >= this.length) {
+            return this.append(value);
+        } else if (!this.length) {
+            this.append(value);
+        }
+
+        const newNode = {value, next: null};
+        const leader = this.traverseToIndex(position - 1);
+        const holdingPointer = leader.next;
+        leader.next = newNode;
+        newNode.next = holdingPointer;
+        this.length++;
+
+    }
+
+    traverseToIndex(index) {
+        let counter = 0;
+        let current = this.head;
+        while (counter !== index) {
+            current = current.next;
+            counter++;
+        }
+
+        return current;
+    }
+
+    printList() {
+        const arr = [];
+        let current = this.head;
+        while (current !== null) {
+            arr.push(current.value);
+            current = current.next;
+        }
+
+        return arr;
+    }
+
+
+    remove(index) {
+        const leader = this.traverseToIndex(index - 1);
+        const deletedNode = leader.next;
+        leader.next = deletedNode.next;
+        this.length--;
+    }
+
 }
 
 const linkedList = new LinkedList(5);
@@ -36,5 +82,9 @@ linkedList.append(15);
 linkedList.append(20);
 linkedList.append(25);
 linkedList.prepend(1);
+linkedList.remove(2);
+// linkedList.insert(1, 99);
+// linkedList.insert(3, 100);
+// linkedList.insert(5, 101);
 
-console.log(linkedList);
+console.log(linkedList.printList());
